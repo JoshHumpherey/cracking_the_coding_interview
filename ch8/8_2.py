317f1ccb659e6a8d47bd7a32d7d225416d8f4204
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-01 15:24:11 -0400

    Finished 8.2

--- ch8/8_2.py
+++ ch8/8_2.py
@@ -1,18 +1,27 @@
 # Cracking the Coding Interview: 8.2
 # Written by Josh Humphrey
+# 1's are walkable while 0's are blockades
 
-room = [[0,0,0,0,0,0,0],
-        [0,1,1,0,0,1,1],
-        [1,1,0,0,0,0,0]]
+grid = [[1,0,0,0,0,0,0],
+        [1,1,1,1,1,1,1],
+        [1,1,0,1,1,0,1]]
 
-def is_there_a_path(room):
-    row = len(room)
-    col = len(room[0])
-    if room == None or row == 0 or col == 0:
+def find_path(grid):
+    if grid is None or len(grid) == 0:
         return None
-    if 
+    path = list()
+    if calc_path(grid, len(grid)-1, len(grid[0])-1, path):
+        return path
+    return None
 
+def calc_path(grid, row, col, path):
+    if row < 0 or col < 0 or not grid[row][col]:
+        return False
+    is_at_origin = (row == 0) and (col == 0)
+    if is_at_origin or calc_path(grid, row-1, col, path) or calc_path(grid, row, col-1, path):
+        path.append((row,col))
+        return True
+    return False
 
-
-
-is_there_a_path(room)
+result = find_path(grid)
+print("Path: " + str(result))
