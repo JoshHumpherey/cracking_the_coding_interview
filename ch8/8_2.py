b9b6bf040d57062c0c61fea806cde7c4c22ecbe2
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-01 15:15:14 -0400

    Working on 8.2

---
+++ ch8/8_2.py
@@ -0,0 +1,18 @@
+# Cracking the Coding Interview: 8.2
+# Written by Josh Humphrey
+
+room = [[0,0,0,0,0,0,0],
+        [0,1,1,0,0,1,1],
+        [1,1,0,0,0,0,0]]
+
+def is_there_a_path(room):
+    row = len(room)
+    col = len(room[0])
+    if room == None or row == 0 or col == 0:
+        return None
+    if 
+
+
+
+
+is_there_a_path(room)
