31c8f7bd573c3c615e55ef87eb04a5edd3689eba
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-07-02 10:54:41 -0400

    Finished 8.7

---
+++ ch8/8_6.py
@@ -0,0 +1,2 @@
+# Cracking the Coding Interview: 8.6
+# Written by Josh Humphrey
---
+++ ch8/8_7.py
@@ -0,0 +1,23 @@
+# Cracking the Coding Interview: 8.7
+# Written by Josh Humphrey
+
+def get_permutations(string):
+    permutations = []
+    if string == None:
+        return None
+    if len(string) == 0:
+        permutations.append("")
+        return permutations
+    string_list = list(string)
+    for i in range(len(string)):
+        before = string_list[:i]
+        after = string_list[i+1:]
+        partials = list(get_permutations(before + after))
+
+        for character in partials:
+            permutations.append(string_list[i] + character)
+    return permutations
+
+test_string = "HelloWorld!"
+result = get_permutations(test_string)
+print(result)
