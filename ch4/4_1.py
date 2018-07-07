a13071dbcc1bb836a1e8221a0457ff4b88bc69c9
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-07 19:24:11 -0400

    Finished 4.1

--- ch4/4_1.py
+++ ch4/4_1.py
@@ -8,17 +8,15 @@ class Node():
         self.adjacents = adjacents
         self.visited = False
 
-def find_path(node1, node2):
-    queue = []
-    queue.append(node1)
-    while(queue != []):
-        current = queue.pop()
-        for adjacent in current.adjacents:
-            if adjacent.visited == False:
-                if adjacent == node2:
-                    return True
-                else:
-                    queue.append(adjacent)
-        current.visited == True
-
+def find_path(Node1, Node2):
+    if Node1 == None or Node2 == None or Node1.visited == True:
+        return False
+    Node1.visited = True
+    if Node1 == Node2:
+        return True
+    exit_var = False
+    for adj in Node1.adjacents:
+        exit_var = find_path(adj, Node2)
+        if exit_var == True:
+            return True
     return False
