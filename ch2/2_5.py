6e2ed243759bf2c2581795a0228192cb88f6cdc9
JoshHumpherey <jhumphrey321@gmail.com>
2018-07-08 15:16:05 -0400

    Finished 2.5

--- ch2/2_5.py
+++ ch2/2_5.py
@@ -7,7 +7,8 @@ class ListNode(object):
         self.val = value
         self.next = None
 
-circle = [1,2,3,4,5,6,7,8,9]
+l1 = [3, 2, 1]
+l2 = [4, 1, 9]
 
 def create_linked_list(the_list):
     creation_node = ListNode(0)
@@ -17,8 +18,6 @@ def create_linked_list(the_list):
         old_node.next = new_node
         old_node = old_node.next
     print("Original Node Values: " + str(the_list))
-    # Modified to return a circular linked list
-    old_node = creation_node.next
     return creation_node.next
 
 def print_linked_list(head):
@@ -29,18 +28,29 @@ def print_linked_list(head):
     print("Printed values: " + str(head_vals))
 
 # ------------- Problem ------------- #
+# Assuming both linked lists are the same size.. #
 
-def detect_large_cycle(head):
-    slowptr = head
-    fastptr = head
-    # Could sub in length of list if given option from LL class
-    for i in range(100):
-        if slowptr == fastptr:
-            return slowptr.val
+def add_lists(l1, l2):
+    orig = ListNode(0)
+    head = orig
+    carry = 0
+    while (l1 != None):
+        sum = carry + l1.val + l2.val
+        print(sum)
+        if sum >= 10:
+            head.val = 1
+            carry = sum - 9
         else:
-            slowptr = slowptr.next
-            fastptr = fastptr.next.next
+            head.val = sum
 
-circularLL = create_linked_list(circle)
-meeting = detect_large_cycle(circularLL)
-print(meeting)
+        head.next = ListNode(0)
+        head = head.next
+        l1 = l1.next
+        l2 = l2.next
+    return orig
+
+l1 = create_linked_list(l1)
+l2 = create_linked_list(l2)
+
+result = add_lists(l1, l2)
+print_linked_list(result)
